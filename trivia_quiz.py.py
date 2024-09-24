daf854480fb3f18283e2f376564b7c2b1c9f7c7b
miri-florsheim <mf127744@gmail.com>
2024-09-24 09:22:48 +0300

    Add files via upload

--- trivia_quiz.py.py
+++ trivia_quiz.py.py
@@ -1,4 +1,21 @@
 
+def run_quiz(questions):
+	score = 0
+	for questions in questions:
+		print(questions["prompt"])
+		for optin in questions["options"]:
+			print(optin)
+		answer = input("Enter your answer (A, B, C, or D): ").upper()
+		if answer == questions["answer"]:
+			print("Very good!!\n")
+			score += 1
+		else:
+			print('Wrong! The correct answer was", question["answer"], "\n')
+
+	print(f"You got {score} out of {len(questions)} questions correct.")
+
+
+# List of quiz questions. Each question is a dictionary.
 questions = [
 	{
 		"prompt": "What is the capital city of France?",
@@ -22,20 +39,5 @@ questions = [
 	}
 ]
 
-def run_quiz(questions):
-	score = 0
-	for questions in questions:
-		print(questions["prompt"])
-		for optin in questions["options"]:
-			print(optin)
-		answer = input("Enter your answer (A, B, C, or D): ").upper()
-		if answer == questions["answer"]:
-			print("Very good!!\n")
-			score += 1
-		else:
-			print('Wrong! The correct answer was", question["answer"], "\n')
-
-	print(f"You got {score} out of {len(questions)} questions correct.")
-
-
+# Run the quiz
 run_quiz(questions)
